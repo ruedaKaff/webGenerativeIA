25cbbde192a3d002c5564f363e485cbfbd065c33
ruedaKaff <jhanru@live.com>
2023-10-20 20:18:48 -0500

    feat: summarizing, entity base routes and service

--- src/routes/generative_routes.js
+++ src/routes/generative_routes.js
@@ -1,17 +1,24 @@
 import { Router } from "express";
 import "dotenv/config.js";
 
+
+//generative sections crud
 import { find, findone } from "../controllers/generative_controller.js";
+//services
+import {summarize, entity} from "../services/NLP_service.js"
 // import { trimMiddleware } from "../common/trimMiddelware.js";
 const generative = Router();
 
 generative.get("/", find);
 
 generative.get("/:id", findone);
+generative.get("/:id", findone);
+generative.get("/", findone);
 
-generative.put("/");
 
-generative.post("/");
+//NLP services
+generative.post("/nlp/summarize",summarize);
+generative.post("/nlp/entity",entity);
 
 generative.delete("/:id");
 
---
+++ src/services/NLP_service.js
@@ -0,0 +1,49 @@
+import  axios  from "axios";
+
+const summarize = async (req, res = response) => {
+  // "NLP-sumarize service here"
+    console.log("Testing nlp");
+  try {
+    const prompt = req.body.prompt;
+
+    reponse = await axios.post("URL_API", {
+      prompt: prompt,
+      // Add paramaters
+    });
+
+    if (response.data && response.data.summary) {
+      const summarizedText = response.data.summary;
+      res.json({ summary: summarizedText });
+    } else {
+      res.status(500).json({ error: "Summarization failed" });
+    }
+  } catch (error) {
+    console.error("Summarization error:");
+    res.status(500).json({ error: "Summarization failed" });
+  }
+};
+
+const entity = async (req, res = response) => {
+  // "NLP-entity recognition service here"
+
+  try {
+    const prompt = req.body.prompt;
+
+    reponse = await axios.post("URL_API", {
+      prompt: prompt,
+      // Add paramaters
+    });
+
+    if (response.data && response.data.summary) {
+      const summarizedText = response.data.summary;
+      res.json({ summary: summarizedText });
+    } else {
+      res.status(500).json({ error: "Summarization failed" });
+    }
+  } catch (error) {
+    console.error("Summarization error:", error);
+    res.status(500).json({ error: "Summarization failed" });
+  }
+};
+
+export { summarize, entity };
