6907a2100eb8c4ad654efecdbe9ea06546f89b1d
ruedaKaff <jhanru@live.com>
2023-11-02 17:20:13 -0500

    feat: added many features of ejs and setting up google auth

--- src/routes/auth_route.js
+++ src/routes/auth_route.js
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import passport from "passport";
+import GoogleStrategy from "passport-google-oidc"
 
 const login = Router();
 
 login.get("/", function (req, res, next) {
-  res.render("home");
+  res.render("login");
 });
+
+login.get("/signup", function (req, res, next) {
+  res.render("signup");
+});
+
+login.get("/federated/google",  passport.authenticate('google'));
 
 export { login };
