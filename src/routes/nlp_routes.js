a814f583253e66d3b169cebe1fa7a37008eedace
ruedaKaff <jhanru@live.com>
2023-11-23 19:34:37 -0500

    feat: NLP entity recognition service

--- src/routes/nlp_routes.js
+++ src/routes/nlp_routes.js
@@ -4,6 +4,8 @@ import "dotenv/config.js";
 import { summarize, entity } from "../services/NLP_service.js";
 import { create } from "../controllers/community_controller.js";
 //NLP services
+
+
 const nlp = Router();
 
 nlp.post("/summarize", summarize);
@@ -11,29 +13,3 @@ nlp.post("/entity", entity);
 nlp.post("/",create)
 
 export { nlp };
-// async function getCompletion(inputs, parameters = null) {
-//     const headers = {
-//       'Authorization': `Bearer ${hfApiKey}`,
-//       'Content-Type': 'application/json',
-//     };
-  
-//     const data = { inputs };
-  
-//     if (parameters !== null) {
-//       data.parameters = parameters;
-//     }
-  
-//     try {
-//       const response = await axios.post(endpointUrl, data, { headers });
-//       return response.data;
-//     } catch (error) {
-//       console.error('Error in getCompletion:', error.message);
-//       throw error;
-//     }
-//   }
-  
-//   async function summarize(input) {
-//     const output = await getCompletion(input);
-//     return output[0]['summary_text'];
-//   }
-  
--- src/services/NLP_service.js
+++ src/services/NLP_service.js
@@ -1,16 +1,17 @@
-import axios from 'axios';
-import { response } from 'express'; // Assuming 'response' is imported from 'express'
-import 'dotenv/config.js';
+import axios from "axios";
+import { response } from "express"; // Assuming 'response' is imported from 'express'
+import "dotenv/config.js";
 
 const hfApiKey = process.env.HF_API_KEY;
-const endpointUrl = process.env.HF_API_SUMMARY_BASE;
-console.log(process.env.HF_API_SUMMARY_BASE);
+const endpointSum = process.env.HF_API_SUMMARY_BASE;
+const endpointNER = process.env.HF_API_NER_BASE;
+
 const summarize = async (req, res = response) => {
   console.log("Testing nlp");
   try {
     const prompt = req.body.prompt;
     const axiosConfig = {
-      headers: { Authorization: `Bearer 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` },
+      headers: { Authorization: `Bearer ${hfApiKey}` },
     };
 
     const axiosData = {
@@ -18,8 +19,8 @@ const summarize = async (req, res = response) => {
       // Add parameters if needed
     };
 
-    const axiosResponse = await axios.post("https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6", axiosData, axiosConfig);
-    console.log('helllo below i should response');
+    const axiosResponse = await axios.post(endpointSum, axiosData, axiosConfig);
+    console.log("helllo below i should response:");
 
     if (axiosResponse.data[0].summary_text) {
       const summarizedText = axiosResponse.data[0].summary_text;
@@ -34,27 +35,89 @@ const summarize = async (req, res = response) => {
   }
 };
 
+// NLP-Entity recognition service here.
+function formatEntities(response) {
+  const { text, entities } = response;
+
+  if (!text || !entities || entities.length === 0) {
+    return text; // No entities or text provided
+  }
+
+  let formattedText = text;
+
+  // Sort entities by start index in descending order
+  entities.sort((a, b) => b.start - a.start);
+
+  // Replace each entity with [entityType]
+  for (const entity of entities) {
+    const { start, end, entity: entityType } = entity;
+    const entityText = text.substring(start, end);
+    const replacement = `[${entityText}:${entityType}]`;
+    formattedText =
+      formattedText.slice(0, start) + replacement + formattedText.slice(end);
+  }
+
+  return formattedText;
+}
+
+function mergeTokens(tokens) {
+  const mergedTokens = [];
+  for (const token of tokens) {
+    if (
+      mergedTokens.length > 0 &&
+      token.entity_group.startsWith("I-") &&
+      mergedTokens[mergedTokens.length - 1].entity_group ===
+        "B-" + token.entity_group.slice(2)
+    ) {
+      // If current token continues the entity of the last one, merge them
+      const lastToken = mergedTokens[mergedTokens.length - 1];
+      lastToken.word += token.word.replace("##", "");
+      lastToken.end = token.end;
+      lastToken.score = (lastToken.score + token.score) / 2;
+    } else {
+      mergedTokens.push(token);
+    }
+  }
+  return mergedTokens;
+}
+
 const entity = async (req, res = response) => {
   // "NLP-entity recognition service here"
   console.log("Testing nlp2");
   try {
-    const prompt = req.body.prompt;
+    const input = req.body.prompt;
+    const axiosConfig = {
+      headers: { Authorization: `Bearer ${hfApiKey}` },
+    };
 
-    response = await axios.post("URL_API", {
-      prompt: prompt,
-      // Add paramaters
-    });
+    const axiosData = {
+      inputs: input,
+      // Add parameters if needed
+    };
 
-    if (response.data && response.data.summary) {
-      const summarizedText = response.data.summary;
-      res.json({ summary: summarizedText });
+    const axiosResponse = await axios.post(endpointNER, axiosData, axiosConfig);
+
+    if (axiosResponse.data && axiosResponse.data.length > 0) {
+      const mergedTokens = mergeTokens(axiosResponse.data);
+      const entities = mergedTokens.map((token) => ({
+        start: token.start,
+        end: token.end,
+        entity: token.entity_group,
+      }));
+
+      // Create an object for better clarity
+      const formattedResponse = { text: input, entities: entities };
+
+      // Send the formatted response
+      res.json({ text: formatEntities(formattedResponse) });
     } else {
-      res.status(500).json({ error: "recognition failed" });
+      res.status(500).json({ error: 'Named Entity Recognition failed - No data received' });
     }
   } catch (error) {
-    console.error("recognition error:");
-    res.status(500).json({ error: "recognition failed" });
+    console.error('Named Entity Recognition error:', error);
+    res.status(500).json({ error: 'Named Entity Recognition failed - Internal server error' });
   }
 };
+
 
 export { summarize, entity };
