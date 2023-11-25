be1658a58cd712b9fd588d272d81c85dbcd5468b
ruedaKaff <jhanru@live.com>
2023-11-25 17:38:50 -0500

    feat: image generation service

--- src/services/IMG_processing_service.js
+++ src/services/IMG_processing_service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import fs from "fs/promises";
 import { response } from "express"; // Assuming 'response' is imported from 'express'
 import "dotenv/config.js";
 
@@ -10,7 +10,7 @@ const endpointSDIF = process.env.HF_API_SDIFFUSION_BASE;
 const captioning = async (req, res = response) => {
   console.log("Testing captioning");
   try {
-    const base64Image = req.file.buffer.toString('base64');
+    const base64Image = req.file.buffer.toString("base64");
 
     const axiosConfig = {
       headers: { Authorization: `Bearer ${hfApiKey}` },
@@ -21,7 +21,11 @@ const captioning = async (req, res = response) => {
       // Add parameters if needed
     };
 
-    const axiosResponse = await axios.post(endpointICAP, axiosData, axiosConfig);
+    const axiosResponse = await axios.post(
+      endpointICAP,
+      axiosData,
+      axiosConfig
+    );
     // console.log(axiosResponse);
     if (axiosResponse.data && axiosResponse.data.length > 0) {
       const generated_text = axiosResponse.data[0].generated_text;
@@ -35,68 +39,75 @@ const captioning = async (req, res = response) => {
   }
 };
 
-
 const generator = async (req, res) => {
-
-  console.log('testing generator');
+  console.log("testing generator");
   try {
     const { prompt, negativePrompt, steps, guidance, width, height } = req.body;
 
     const params = {
-      "negative_prompt": negativePrompt,
-      "num_inference_steps": steps,
-      "guidance_scale": guidance,
-      "width": width,
-      "height": height,
+      negative_prompt: negativePrompt,
+      num_inference_steps: steps,
+      guidance_scale: guidance,
+      width: width,
+      height: height,
     };
     const uniquePrompt = `${prompt}_${Math.floor(Date.now() / 1000)}`;
     const output = await imgGEN(uniquePrompt, params);
 
     if (output) {
-      // Process the output as needed
-      return output;
+       res.setHeader('Content-Type', 'image/jpeg'); // Adjust the content type based on the image format
+       res.setHeader('Content-Disposition', 'inline; filename=image.jpg'); // Specify the filename
+
+      // Send the binary data as the response
+      res.send(output);
+      
     } else {
-      return null;  // Handle case where output is null
+      return null; // Handle case where output is null
     }
   } catch (error) {
-    console.error("Error in generator:", error);
+    console.error("Error in generator:");
     return null;
   }
 };
 
-
-const imgGEN = async (inputs, parameters = null, ENDPOINT_URL = endpointSDIF) => {
-  console.log('testing generator, went to imgGen');
-  console.log(endpointSDIF);
+const imgGEN = async (
+  inputs,
+  parameters = null,
+  ENDPOINT_URL = endpointSDIF
+) => {
+ 
 
   try {
     const headers = {
-      "Authorization": `Bearer ${hfApiKey}`,
+      Authorization: `Bearer ${hfApiKey}`,
       "Content-Type": "application/json",
     };
 
-    const data = { "inputs": inputs };
+    const data = { inputs: inputs };
 
     if (parameters) {
       data.parameters = parameters;
     }
 
-    const response = await axios.post(ENDPOINT_URL, data, { headers });
+    const response = await axios.post(ENDPOINT_URL, data, { headers, responseType: 'arraybuffer' });
 
     if (response.status === 200) {
-  
-     const b64 = atob(response.data)
-      console.log(b64);
-      // You can also save it to a file or display it in any other way
-      return response.data;
+      // Convert the binary data to a Buffer
+      const buffer = Buffer.from(response.data);
+
+      // Save the Buffer to a file (adjust the filename and path as needed)
+      const filename = "output.jpg";
+      await fs.writeFile(filename, buffer);
+
+      console.log(`Image saved to ${filename}`);
+      return buffer;
     } else {
-      return null;  // Handle error cases appropriately
+      return null; // Handle error cases appropriately
     }
   } catch (error) {
     console.error("Error in getCompletion:", error);
     return null;
   }
 };
-
 
 export { captioning, generator };
