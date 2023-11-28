7e144f546aebdd56c9c5ae497c2dba44eddb2794
ruedaKaff <jhanru@live.com>
2023-11-28 00:35:22 -0500

    Fixed: mysql2/promises, response handle community and generative controllers

--- src/controllers/community_controller.js
+++ src/controllers/community_controller.js
@@ -2,77 +2,59 @@ import { response } from "express";
 import { pool } from "../common/connection.js";
 import "dotenv/config.js";
 
-const find = (req, res = response) => {
-  console.log('Im in fin with get community');
-  pool.query(
-    `SELECT community.*, users.email
+const find = async (req, res = response) => {
+  try {
+    const [result] = await pool.query(`
+      SELECT community.*, users.email
       FROM community
       INNER JOIN users ON community.user_id = users.id;
-      
-      `,
-    [],
-    function (err, result, fields) {
-      // Check for errors during the query
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
-
-      // Check the length of the result before accessing its properties
-      if (result && result.length > 0) {
-        console.log(result);
-        res.json(result);
-      } else {
-        res.status(404).json({ response: process.env.DEFAULT });
-      }
-    }
-  );
+    `);
+    console.log("Query result:", result);
+    return result && result.length > 0
+      ? res.json(result)
+      : res.status(404).json({ response: process.env.DEFAULT });
+  } catch (error) {
+    console.error("Error executing query:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
-const findone = (req, res = response) => {
-  pool.query(
-    `SELECT community.*, users.email
+const findone = async (req, res = response) => {
+  try {
+    const [result] = await pool.query(`
+      SELECT community.*, users.email
       FROM community
       INNER JOIN users ON community.user_id = users.id
-      WHERE id_community= ?
-      `,
-    // Los parámetros para la consulta se obtienen del parámetro de ruta 'documento' de la solicitud
-    [req.params.id],
-    function (err, result, fields) {
-      // Check for errors during the query
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
+      WHERE id_community= ?;
+    `, [req.params.id]);
 
-      // Check the length of the result before accessing its properties
-      if (result && result.length > 0) {
-        res.json(result);
-      } else {
-        res.status(404).json({ response: process.env.DEFAULT });
-      }
-    }
-  );
+    return result && result.length > 0
+      ? res.json(result)
+      : res.status(404).json({ response: process.env.DEFAULT });
+  } catch (error) {
+    console.error("Error executing query:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
-const create = (req, res = response) => {
-  console.log('Hi, me dejaron crear un post');
-  const { user_id, input, output, outimage, model_type, username } = req.body;
- 
-  pool.query(
-    // Realiza una consulta SQL para insertar un nuevo community item en la tabla community con NOW() para FechaHoraAuditoria
-    `INSERT INTO community ( user_id, input, output, outimage, model_type, username, updated_at)
-    VALUES (?, ?, ?, ?, ?, ?, NOW());
-    `,
-    // Los valores para la consulta se obtienen de las variables extraídas del cuerpo de la solicitud
-    [user_id, input, output, outimage, model_type, username],
-    function (err, result, fields) {
-      // Si ocurre un error, devuelve el mensaje de error; de lo contrario, devuelve el resultado de la inserción
-      err 
-      ? res.status(500).json({ error: err.message }) 
-      : res.status(200).json({ message: 'Item creado exitosamente' });
-    }
-  );
+
+const create = async (req, res = response) => {
+  try {
+    console.log('Hi, me dejaron crear un post');
+    const { user_id, input, output, outimage, model_type, username } = req.body;
+
+    await pool.query(
+      `INSERT INTO community (user_id, input, output, outimage, model_type, username, updated_at)
+      VALUES (?, ?, ?, ?, ?, ?, NOW());
+      `,
+      [user_id, input, output, outimage, model_type, username]
+    );
+
+    return res.status(200).json({ message: 'Item created successfully' });
+  } catch (error) {
+    console.error("Error executing query:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export { find, findone, create};
--- src/controllers/generative_controller.js
+++ src/controllers/generative_controller.js
@@ -2,33 +2,41 @@ import { response } from "express";
 import { pool } from "../common/connection.js";
 import "dotenv/config.js";
 
-const find = (req, res = response) => {
-  pool.query(
-    `
-      SELECT * FROM sections;
-    `,
-    [],
-    function (err, result, fields) {
-      // Si no se encuentran resultados, devuelve un código de estado 404 y un mensaje de error
-      result.length == 0
-        ? res.status(404).json({ response: process.env.DEFAULT })
-        : res.json(result);
-    }
-  );
+const find = async (req, res = response) => {
+  try {
+    const [result] = await pool.query(
+      `SELECT * FROM sections;`
+    );
+    //debugin
+    console.log("Query result:", result);
+    result.length > 0
+      ? res.json(result)
+      : res.status(404).json({ response: process.env.DEFAULT });
+  
+    } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
-const findone = (req, res = response) => {
-  pool.query(
-    `SELECT * FROM sections 
-     WHERE id = ?`,
-    [req.params.id],
 
-    function (err, result, fields) {
-      result.length == 0
-        ? res.status(404).json({ response: process.env.DEFAULT })
-        : res.json(result);
-    }
-  );
+const findone = async (req, res = response) => {
+  try {
+    const [result] = await pool.query(
+      `SELECT * FROM sections WHERE id = ?`,
+      [req.params.id]
+    );
+
+    // Debugging
+    console.log("Query result:", result);
+
+    result.length === 0
+      ? res.status(404).json({ response: process.env.DEFAULT })
+      : res.json(result);
+  } catch (error) {
+    console.error("Error executing query:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export { find, findone };
